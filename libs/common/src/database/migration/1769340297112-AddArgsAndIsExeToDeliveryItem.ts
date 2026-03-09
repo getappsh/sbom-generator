4081136486cd91472650ba28de7d0ff474c730dd
mataniss <matanissler@gmail.com>
2026-03-09 11:43:58 +0200

    fix migration

--- libs/common/src/database/migration/1769340297112-AddArgsAndIsExeToDeliveryItem.ts
+++ libs/common/src/database/migration/1769340297112-AddArgsAndIsExeToDeliveryItem.ts
@@ -4,13 +4,13 @@ export class AddArgsAndIsExeToDeliveryItem1769340297112 implements MigrationInte
     name = 'AddArgsAndIsExeToDeliveryItem1769340297112'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "delivery_item" ADD "is_executable" boolean NOT NULL DEFAULT false`);
-        await queryRunner.query(`ALTER TABLE "delivery_item" ADD "arguments" text`);
+        await queryRunner.query(`ALTER TABLE "delivery_item" ADD COLUMN IF NOT EXISTS "is_executable" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "delivery_item" ADD COLUMN IF NOT EXISTS "arguments" text`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "delivery_item" DROP COLUMN "arguments"`);
-        await queryRunner.query(`ALTER TABLE "delivery_item" DROP COLUMN "is_executable"`);
+        await queryRunner.query(`ALTER TABLE "delivery_item" DROP COLUMN IF EXISTS "arguments"`);
+        await queryRunner.query(`ALTER TABLE "delivery_item" DROP COLUMN IF EXISTS "is_executable"`);
     }
 
 }
